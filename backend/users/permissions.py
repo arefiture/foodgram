bc6107e2e79f19350d7c600250bc60b4ce4a5e69
Aleksandr Rakov <alexrinko@hotmail.com>
2024-09-23 11:30:14 +0500

    Немного изменений в логике прав

--- backend/users/permissions.py
+++ backend/users/permissions.py
@@ -1,9 +1,12 @@
 from djoser.permissions import CurrentUserOrAdminOrReadOnly
 from rest_framework.permissions import (
     BasePermission,
+    IsAuthenticated,
     IsAuthenticatedOrReadOnly,
     SAFE_METHODS
 )
+
+CHANGE_METHODS = ('PUT', 'PATCH', 'DELETE')
 
 
 class CurrentUserOrAdminOrReadOnly(
@@ -14,11 +17,18 @@ class CurrentUserOrAdminOrReadOnly(
 
 
 class ReadOnly(BasePermission):
+    """Доступ к странице только для просмотра."""
     def has_permission(self, request, view):
         return request.method in SAFE_METHODS
 
 
-class IsAuthor(BasePermission):
+class IsAuthor(IsAuthenticated):
+
+    def has_permission(self, request, view):
+        return (
+            request.method in CHANGE_METHODS
+            and super().has_permission(request, view)
+        )
 
     def has_object_permission(self, request, view, obj):
         return obj.author == request.user
